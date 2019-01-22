396867005a19dcde4b6c1937ae6e7f1e1c42e87b
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-01-22 15:48:47 -0600

    Adding gitignore

--- BCOG200/Labs/2-1/variables.py
+++ BCOG200/Labs/2-1/variables.py
@@ -16,16 +16,16 @@ C = 4.0
     print(Z+B)
 
 2. what do the following statements do:
-    B += 5
-    D = C ** 2
-    D = B / C
-    D = B // C
-    D = B % C
+    B += 5 # B = 35
+    D = C ** 2 # D = 16.0
+    D = B / C # D = 7.5
+    D = B // C # D = 4.0
+    D = B % C $ D = 2.0
 
 3. What is the difference between the following two statements?
-    B = 20
-    B == 20
+    B = 20 # Assignment expr
+    B == 20 # Bool expr
 
 4. if you wanted to print out X and B, resulting in "DOG 30", how would you do that?
-
+print('%s %d' % (X, B))
 '''
